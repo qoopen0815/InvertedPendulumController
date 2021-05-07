a897391219df9292f93ae55674c978f0cea41441
TomohiroKuwano <ku_engineer_wa@yahoo.com>
2021-05-07 17:42:14 +0900

    Check motor library

--- PlatformIO/state_feedback_controller/src/main.cpp
+++ PlatformIO/state_feedback_controller/src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
+#include <RotaryEncoder.h>
+#include <TB6612FNG.h>
+
+Tb6612fng motors(27, 12, 13, 25, 32, 33, 26);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  motors.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  for (int i=0; i<360; i++) {
+    float speed = sin((double)i*2*3.14/360.0);
+    motors.drive(speed, -speed);
+    delay(50);
+  }
 }
