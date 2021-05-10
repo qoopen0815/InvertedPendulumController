08c5707428ae93334ef82dd244616192b220f012
TomohiroKuwano <ku_engineer_wa@yahoo.com>
2021-05-10 23:44:59 +0900

    Add Encoder [motorA, motorB]

--- PlatformIO/state_feedback_controller/src/main.cpp
+++ PlatformIO/state_feedback_controller/src/main.cpp
@@ -7,6 +7,19 @@
 #include <TB6612FNG.h>
 #include <Wire.h>
 
+#define STBY 27
+#define AIN1 12
+#define AIN2 13
+#define PWMA 25
+#define BIN1 32
+#define BIN2 33
+#define PWMB 26
+
+#define ENC1A 36
+#define ENC1B 39
+#define ENC2A 34
+#define ENC2B 35
+
 #define TESTPINA 5
 #define TESTPINB 14
 
@@ -15,19 +28,25 @@ const double dt = 1.0/rate;
 
 Kalman inclinometer;
 MPU6050 imu;
-Tb6612fng motors(27, 12, 13, 25, 32, 33, 26);
+RotaryEncoder encoder1(ENC1A, ENC1B, RotaryEncoder::LatchMode::TWO03);
+RotaryEncoder encoder2(ENC2A, ENC2B, RotaryEncoder::LatchMode::TWO03);
+Tb6612fng motors(STBY, AIN1, AIN2, PWMA, BIN1, BIN2, PWMB);
 
+// Inclinometer parameter
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 int16_t tempRaw;
-
 double kalAngleY; // Calculated angle using a Kalman filter
+
+// Encoder parameter
+double motorA;
+double motorB;
 
 // double theta[2] = {0.0, 0.0};  // 0: new, 1: old
 
-void updateInclinometer( void *pvParameters );
+void readInclinometer( void *pvParameters );
+void readEncoder( void *pvParameters );
 void updateMotorControl( void *pvParameters );
-void TaskView( void *pvParameters );
 
 void setup() {
   pinMode(TESTPINA, OUTPUT);
@@ -48,29 +67,28 @@ void setup() {
   double pitch = atan2(-ax, az) * RAD_TO_DEG;
   inclinometer.setAngle(pitch);
 
-
   // Now set up two tasks to run independently.
-  xTaskCreateUniversal( updateInclinometer,
-                        "updateInclinometer",   // A name just for humans
+  xTaskCreateUniversal( readInclinometer,
+                        "readInclinometer",   // A name just for humans
                         1000,           // This stack size can be checked & adjusted by reading the Stack Highwater
                         NULL,
-                        3,              // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+                        2,              // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
                         NULL,
                         PRO_CPU_NUM );
+                        
+  xTaskCreateUniversal( readEncoder,
+                        "readEncoder",   // A name just for humans
+                        1000,           // This stack size can be checked & adjusted by reading the Stack Highwater
+                        NULL,
+                        1,              // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+                        NULL,
+                        APP_CPU_NUM );
 
   xTaskCreateUniversal( updateMotorControl,
                         "updateMotorControl",   // A name just for humans
                         800,           // This stack size can be checked & adjusted by reading the Stack Highwater
                         NULL,
-                        2,              // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-                        NULL,
-                        PRO_CPU_NUM );
-
-  xTaskCreateUniversal( TaskView,
-                        "TaskView",     // A name just for humans
-                        800,            // This stack size can be checked & adjusted by reading the Stack Highwater
-                        NULL,
-                        1,              // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+                        3,              // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
                         NULL,
                         PRO_CPU_NUM );
 }
@@ -84,7 +102,7 @@ void loop()
 /*---------------------- Tasks ---------------------*/
 /*--------------------------------------------------*/
 
-void updateInclinometer(void *pvParameters)  // This is a task.
+void readInclinometer(void *pvParameters)  // This is a task.
 {
   (void) pvParameters;
 
@@ -93,9 +111,24 @@ void updateInclinometer(void *pvParameters)  // This is a task.
     imu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
     double pitch = atan2(-ax, az) * RAD_TO_DEG;
     double gyroY = gy / 131.0;  // Convert to deg/s
-    kalAngleY = inclinometer.getAngle(pitch, gyroY, 30);
+    kalAngleY = inclinometer.getAngle(pitch, gyroY, 2);
     digitalWrite(TESTPINA, !(digitalRead(TESTPINA)));
     vTaskDelay(2);  // one tick delay (15ms) in between reads for stability
+  }
+}
+
+void readEncoder(void *pvParameters)  // This is a task.
+{
+  (void) pvParameters;
+
+  for (;;) // A Task shall never return or exit.
+  {
+    encoder1.tick();
+    encoder2.tick();
+    motorA = (double)encoder1.getPosition() * 3.14/600.;
+    motorB = (double)encoder2.getPosition() * 3.14/600.;
+    digitalWrite(TESTPINB, !(digitalRead(TESTPINB)));
+    delayMicroseconds(50);
   }
 }
 
@@ -105,21 +138,8 @@ void updateMotorControl(void *pvParameters)  // This is a task.
 
   for (;;)
   {
-    // Serial.println("Do");
     float speed = sin(0.0*3.14/180.0);
     motors.drive(speed, speed);
-    digitalWrite(TESTPINB, !(digitalRead(TESTPINB)));
     vTaskDelay(8);  // one tick delay (15ms) in between reads for stability
-  }
-}
-
-void TaskView(void *pvParameters)  // This is a task.
-{
-  (void) pvParameters;
-
-  for (;;)
-  {
-    // Serial.println("Do");
-    vTaskDelay(10);  // one tick delay (15ms) in between reads for stability
   }
 }
